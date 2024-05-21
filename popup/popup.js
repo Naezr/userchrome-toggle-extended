79d38974c255d15294ea21a77916005f0720ef4a
Naezr <95460152+Naezr@users.noreply.github.com>
2024-05-21 22:42:09 +0700

    popup fixes
    Now when you open a popup, you can see the enabled values

--- popup/popup.js
+++ popup/popup.js
@@ -11,26 +11,28 @@ async function documentLoaded() {
     const settings = await browser.storage.local.get(['toggles', 'general']);
     const toggles = settings.toggles;
     const type = settings.general.allowMultiple ? 'checkbox' : 'radio';
+    let status = true;
 
     for (let i = 0; i < toggles.length; i++) {
-        if (!toggles[i].enabled)
-            continue;
-
-        addSelectRow(type, i + 1, toggles[i].name)
+        let state = toggles[i].state;
+        if (state) status = false;
+        if (!toggles[i].enabled) continue;
+        addSelectRow(type, i + 1, toggles[i].name, state)
     }
 
     if (type == "radio") {
-        addSelectRow(type, 'none', 'None')
+        addSelectRow(type, 'none', 'None', status)
     }
 }
 
-function addSelectRow(type, styleId, name) {
+function addSelectRow(type, styleId, name, state) {
     let container = document.createElement('div');
     let selector = document.createElement('input');
     container.appendChild(selector);
     selector.type = type;
     selector.id = `select-${styleId}`;
     selector.name = 'style';
+    selector.checked = state
     selector.addEventListener('change', event => {
         console.log('Changed', event.target.id);
         browser.extension.getBackgroundPage().userToggle(event.target.id, event.target.checked);
